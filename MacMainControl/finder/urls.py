71c0148954e70a5f68d9f9987af0e689fce7c334
guozhijie <gzj1000xp@hotmail.com>
2018-12-21 17:19:24 +0800

    finder

--- MacMainControl/finder/urls.py
+++ MacMainControl/finder/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.list),
     path('cat/<int:script_id>/', views.get_content, name='get script content'),
+    path('execute/<int:script_id>/', views.execute_script, name='execute script'),
 ]
--- MacMainControl/finder/views.py
+++ MacMainControl/finder/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from .models import FinderScript
 from django.template import loader
 from django.shortcuts import render
+import os
 
 
 def index(request):
@@ -28,4 +29,21 @@ def get_content(request, script_id):
 def list(request):
     script_list = FinderScript.objects.filter(name__startswith='finder')
     output = ','.join([q.name for q in script_list])
+    return HttpResponse(output)
+
+
+def execute_script(request, script_id):
+    script_name = FinderScript.objects.get(id=script_id).name
+    script_path = FinderScript.objects.get(id=script_id).path
+    filename = script_path + script_name
+
+    print(filename)
+    con=filename
+
+    try:
+        os.system('osascript ' + con)
+        output = "success"
+    except 'Exception':
+        output = "fail"
+
     return HttpResponse(output)
